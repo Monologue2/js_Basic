642443693371a9db75cfe9e648c4e9855c8c3468
Monologue2 <seyoumoon@gmail.com>
2023-02-28 17:21:55 +0900

    add class syntax

--- basic syntax/13_class.js
+++ basic syntax/13_class.js
@@ -1,2 +1,33 @@
 #!/usr/bin/env node
 
+//클래스 문법으로 바뀌었으나, 프로토타입 기반으로 동작한다.
+class Human{
+    constructor(type = 'human') {
+        this.type = type;
+    }
+
+    static isHuman(human){ //클래스 함수
+        //instanceof 연산자 --> 프로토타입 체인 어딘가에 존재하는지 판별함
+        return human instanceof Human;
+    }
+
+    breathe(){//메소드
+        alert ('호아암');
+    }
+}
+
+class Zero extends Human {
+    constructor(type, firstName, secondName){
+        super(type);//상속하는 부분
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    sayName(){
+        super.breathe();//메소드 오버로딩
+        alert(`${this.firstName} ${this.secondName}`);
+    }
+}
+
+const newZero = new Zero('human', 'Zero', 'choi');
+console.log(Human.isHuman(newZero));
